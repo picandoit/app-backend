acc3883ae0d1da46b4fb69b804393f089790c448
Samet Orbuk <orbuk60@gmail.com>
2024-12-11 13:10:14 +0300

    Initial commit

---
+++ src/test/java/com/example/kahootcl/KahootclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kahootcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KahootclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
