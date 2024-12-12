7c199a639c5a076b6c21e36fc09aececba7a3378
Samet Orbuk <orbuk60@gmail.com>
2024-12-12 09:20:24 +0300

    Revert "ExceptionHandler anotation added"
    This reverts commit 4f1b03553118a410ab4097124dab2b4122fd3ae9.

--- src/main/java/com/example/kahootcl/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/kahootcl/exception/GlobalExceptionHandler.java
@@ -3,19 +3,17 @@ package com.example.kahootcl.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
 
-    @ExceptionHandler
     public ResponseEntity<KahootExceptionResponse> exceptionHandler(KahootException exception){
         KahootExceptionResponse response = new KahootExceptionResponse(exception.getMessage(), exception.getStatus(), System.currentTimeMillis());
         return new ResponseEntity<>(response,exception.getStatus());
     }
 
-    @ExceptionHandler
+
     public ResponseEntity<KahootExceptionResponse> generalExceptionHandler(Exception exception){
         KahootExceptionResponse response = new KahootExceptionResponse(exception.getMessage(), HttpStatus.BAD_REQUEST, System.currentTimeMillis());
         return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
