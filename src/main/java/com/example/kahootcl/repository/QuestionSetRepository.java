b76c0ced6b9e8eae3a12c809715a228c5246c407
Sena Kokoz Pazar <senakokoz@gmail.com>
2024-12-16 01:55:08 +0300

    Create QuestionSetRepository.java

---
+++ src/main/java/com/example/kahootcl/repository/QuestionSetRepository.java
@@ -0,0 +1,7 @@
+package com.example.kahootcl.repository;
+
+import com.example.kahootcl.entity.QuestionSet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionSetRepository extends JpaRepository<QuestionSet, Long> {
+}
